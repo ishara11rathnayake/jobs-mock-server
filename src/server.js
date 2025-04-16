aa958446a4d9247f3ae20e7c1925757f9081412a
Ishara Rathnayake <ishara11rathnayake@gmail.com>
2025-04-17 00:47:35 +1000

    fix issues

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const router = jsonServer.router('src/data/db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
-server.use(cors({ origin: 'https://67ffc038f677b300085a3279--precious-tarsier-fd6026.netlify.app' }));
+server.use(cors({ origin: 'https://job-finder-vue.netlify.app' }));
 server.use(middlewares);
 server.use(router);
 server.listen(port, () => {
